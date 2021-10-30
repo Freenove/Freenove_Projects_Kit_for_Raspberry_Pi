926f7d9ef4c08be8fdf50ca091b359461898acc6
Suhayl <838119509@qq.com>
2021-10-30 17:49:20 +0800

    ws281x adds support for pi400
    lib ws281x adds support for pi400

--- Libs/C-Libs/libWS281X/rpihw.c
+++ Libs/C-Libs/libWS281X/rpihw.c
@@ -53,6 +53,16 @@
 
 static const rpi_hw_t rpi_hw_info[] = {
     //
+    // Raspberry Pi 400
+    //
+    {
+        .hwver = 0xc03130,
+        .type = RPI_HWVER_TYPE_PI4,
+        .periph_base = PERIPH_BASE_RPI4,
+        .videocore_base = VIDEOCORE_BASE_RPI2,
+        .desc = "Pi 400 - 4GB v1.0"
+    },
+    //
     // Raspberry Pi 4
     //
     {
@@ -98,11 +108,56 @@ static const rpi_hw_t rpi_hw_info[] = {
         .desc = "Pi 4 Model B - 4GB v1.2"
     },
     {
+        .hwver = 0xb03114,
+        .type = RPI_HWVER_TYPE_PI4,
+        .periph_base = PERIPH_BASE_RPI4,
+        .videocore_base = VIDEOCORE_BASE_RPI2,
+        .desc = "Pi 4 Model B - 2GB v1.4"
+    },
+    {
         .hwver = 0xD03114,
         .type = RPI_HWVER_TYPE_PI4,
         .periph_base = PERIPH_BASE_RPI4,
         .videocore_base = VIDEOCORE_BASE_RPI2,
-        .desc = "Pi 4 Model B - 8GB v1.2"
+        .desc = "Pi 4 Model B - 8GB v1.4"
+    },
+    {
+        .hwver = 0xc03114,
+        .type = RPI_HWVER_TYPE_PI4,
+        .periph_base = PERIPH_BASE_RPI4,
+        .videocore_base = VIDEOCORE_BASE_RPI2,
+        .desc = "Pi 4 Model B - 4GB v1.4"
+    },
+    //	
+    // Compute Module 4
+    //
+    {
+        .hwver = 0xa03140,
+        .type = RPI_HWVER_TYPE_PI4,
+        .periph_base = PERIPH_BASE_RPI4,
+        .videocore_base = VIDEOCORE_BASE_RPI2,
+        .desc = "Compute Module 4 v1.0 eMMC"
+    },
+    {
+        .hwver = 0xb03140,
+        .type = RPI_HWVER_TYPE_PI4,
+        .periph_base = PERIPH_BASE_RPI4,
+        .videocore_base = VIDEOCORE_BASE_RPI2,
+        .desc = "Compute Module 4 v1.0 Lite"
+    },
+    {
+        .hwver = 0xc03140,
+        .type = RPI_HWVER_TYPE_PI4,
+        .periph_base = PERIPH_BASE_RPI4,
+        .videocore_base = VIDEOCORE_BASE_RPI2,
+        .desc = "Compute Module 4 v1.0 WiFi"
+    },
+    {
+        .hwver = 0xd03140,
+        .type = RPI_HWVER_TYPE_PI4,
+        .periph_base = PERIPH_BASE_RPI4,
+        .videocore_base = VIDEOCORE_BASE_RPI2,
+        .desc = "Compute Module 4 v1.0 WiFi 8GB"
     },
     //
     // Model B Rev 1.0
